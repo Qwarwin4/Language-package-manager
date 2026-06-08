13a5660ed170372c0f206772e4e1ea39876c0d6f
Qwarwin <qwarwin@gmail.com>
2026-06-08 12:21:32 +0300

    utils.py

---
+++ utils.py
@@ -0,0 +1,23 @@
+class Colors:
+    ENDC    = '\033[0m'
+    BOLD    = '\033[1m'
+    OKBLUE  = '\033[94m'
+    OKCYAN  = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL    = '\033[91m'
+
+def log_info(msg):
+    print(f"{Colors.OKCYAN}[ℹ]  {msg}{Colors.ENDC}")
+
+def log_success(msg):
+    print(f"{Colors.OKGREEN}[✔]  {msg}{Colors.ENDC}")
+
+def log_error(msg):
+    print(f"{Colors.FAIL}[✘]  {msg}{Colors.ENDC}")
+
+def log_warn(msg):
+    print(f"{Colors.WARNING}[⚠]  {msg}{Colors.ENDC}")
+
+def log_action(cmd):
+    print(f"{Colors.OKBLUE}[🚀] {cmd}{Colors.ENDC}")
